daac0f4d8e380c88b532564b796a68801cb8b623
lamsyahbilal <lamsyahbilal1996@gmail.com>
2024-08-23 13:28:41 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navebar from './components/Navebar/Navebar';
+import Hero from './components/Hero/Hero';
+
+const App = () => {
+  return (
+    <div>
+      <Navebar/>
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
